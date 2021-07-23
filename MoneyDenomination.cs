54b3105f6b02625bee5aa88a7d2080aa90b723e5
Christian de la Cruz <chrisdlc119@outlook.com>
2021-07-23 17:53:55 -0500

    Set money denomination

---
+++ MoneyDenomination.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyChangeDue
+{
+    static class MoneyDenomination
+    {
+		public static List<decimal> SetMoneyDenomination(string countryCode)
+		{
+			List<decimal> currencies = new();
+
+			if (countryCode.ToUpper() == "MX")
+			{
+				currencies.Add(100);
+				currencies.Add(50);
+				currencies.Add(20);
+				currencies.Add(10);
+				currencies.Add(5);
+				currencies.Add(1);
+				currencies.Add(0.5m);
+			}
+			else if (countryCode.ToUpper() == "US")
+			{
+				currencies.Add(100);
+				currencies.Add(50);
+				currencies.Add(20);
+				currencies.Add(10);
+				currencies.Add(5);
+				currencies.Add(2);
+				currencies.Add(1);
+				currencies.Add(0.5m);
+				currencies.Add(0.25m);
+				currencies.Add(0.1m);
+				currencies.Add(0.05m);
+				currencies.Add(0.01m);
+			}
+			else
+			{
+				return null;
+			}
+
+			return currencies;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MoneyChangeDue
 {
     class Program
     {
-        static void Main(string[] args)
+		public static List<decimal> currencies;
+		static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			// Define country and currencies for global usage
+			Console.Write("Set country code for currency (MX, US): ");
+			string countryCode = Console.ReadLine();
+
+			currencies = MoneyDenomination.SetMoneyDenomination(countryCode);
+
+			// Validate country code
+			if (currencies == null)
+			{
+				Console.WriteLine("Invalid country code. Please make sure to enter one of the country codes between parenthesis.");
+				Environment.Exit(0);
+			}
+		}
     }
 }
